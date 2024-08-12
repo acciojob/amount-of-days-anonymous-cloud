8e020ded5b62dddbaa7687df11cb7cf1a85dfcd7
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2024-08-12 15:01:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let daysOfAYear(){
+
+	let days ;
+
+	if(days % 4 ==0 ){
+		console.log(366);
+	}
+	else{
+console.log(365)}
+	
+}
+
+daysOfAYear(2022);
+daysOfAYear(2021);
+daysOfAYear(1900);
+daysOfAYear(2000);
