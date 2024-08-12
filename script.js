8d27a75a334b92b9e5e00a99734f8cc181d6ca5e
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2024-08-12 15:06:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //your JS code here. If required.
-let daysOfAYear(){
+let daysOfAYear(days){
 
-	let days ;
+	
 
-	if(days % 4 ==0 ){
-		console.log(366);
+	if((year % 4 ===0 && year %100!==0) || (year % 400 === 0)){
+		return(366);
 	}
 	else{
-console.log(365)}
+return(365)}
 	
 }
 
-daysOfAYear(2022);
-daysOfAYear(2021);
-daysOfAYear(1900);
-daysOfAYear(2000);
+console.log(daysOfAYear(2022)); 
+console.log(daysOfAYear(2024)); 
+console.log(daysOfAYear(1900)); 
+console.log(daysOfAYear(2000)); 
